198e1bd96470344457df274d10fe9370de1f3b2d
Yuriy Dubinin <yuradubinin100@gmail.com>
2025-11-28 15:01:03 +0300

    Work in progress

--- src/PluginInterface.cpp
+++ src/PluginInterface.cpp
@@ -39,7 +39,7 @@ extern "C" void CreateReport(rapidjson::Value& request,
     std::vector<ServerLog> log_vector;
 
     try {
-        server->GetLogs(from_two_weeks_ago, to, "ALL", "request", &log_vector);
+        server->GetLogs(from_two_weeks_ago, to, "ALL", "REQUEST", &log_vector);
     } catch (const std::exception& e) {
         std::cerr << "[DailyLogsReportInterface]: " << e.what() << std::endl;
     }
